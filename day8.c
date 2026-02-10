79e30b0fc119a4dc347f3d99e2c43fa55b493d0b
Vishwash-chaudahry <vishwaschaudhary456@gmail.com>
2026-02-10 10:09:20 +0530

    day 8

---
+++ day8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+long long power(int base, int exp) {
+    if (exp == 0) {
+        return 1;
+    }
+    return base * power(base, exp - 1);
+}
+
+int main() {
+    int a, b;
+    
+    if (scanf("%d %d", &a, &b) != 2) {
+        return 1;
+    }
+    
+    printf("%lld\n", power(a, b));
+    
+    return 0;
+}
